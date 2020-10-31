8de2b45e858c8bc52baa7b04e9f0d4429e16cd46
Syahrizal Setiawan <syahrizal.setiawan11@gmail.com>
2020-10-31 08:45:32 +0700

    add component sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,91 @@
+<template>
+	<nav class="sideNav">
+		<a class="closeBtn"><img src="../assets/sidebar/hamburger.svg" @click.prevent="collapsible()"></a>
+		<div class="links">
+			<a href="#"><img class="__icon active" src="../assets/sidebar/home.svg"></a>
+			<a href="#"><img class="__icon" src="../assets/sidebar/archive.svg"></a>
+			<a href="#"><img class="__icon" src="../assets/sidebar/bookmark.svg"></a>
+			<a href="#"><img class="__icon" src="../assets/sidebar/message.svg"></a>
+		</div>
+		<div class="bottom-links">
+			<a href="#"><img src="../assets/sidebar/cog.svg"></a>
+			<div class="line"></div>
+			<a href="#"><img src="../assets/sidebar/flag-id.svg"></a>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+		name: 'sidebar',
+		data: () => ({
+			isCollapsible: false
+		}),
+		methods: {
+			collapsible () {
+				this.isCollapsible = !this.isCollapsible
+				document.querySelector('.sideNav').style.width = this.isCollapsible ? '300px' : '72px'
+			}
+		}
+}
+</script>
+
+<style lang="scss" scoped>
+.sideNav {
+  height: 100vh;
+  width: 72px;
+	display: flex;
+	flex-direction: column;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color:#0077B5;
+  overflow-x: hidden;
+  padding-top: 60px;
+  transition: 0.5s;
+	justify-content: center;
+	.closeBtn {
+		position: absolute;
+		text-align: center;
+		width: 100%;
+		top: 0;
+		font-size: 30px;
+	}
+	.links {
+		margin-top: 0px;
+		display: flex;
+		flex-direction: column;
+		height: 80%;
+		a {
+			margin: 0 auto;
+			padding: 8px 8px 8px 8px;
+			text-decoration: none;
+			font-size: 25px;
+			color: #ffff;
+			display: block;
+			transition: 0.3s;
+		}
+	}
+	.bottom-links {
+		display: flex;
+		flex-direction: column;
+		a {
+			margin: 0 auto;
+		}
+		.line {
+			width: 24px;
+			height: 1px;
+			margin: 13px 0;
+			align-self: center;
+			background: #949494;
+		}
+	}
+}
+
+.active {
+	background-color: #018eda;
+	border-radius: 30%;
+	padding: 6px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,16 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      clipped-right
-      flat
-      height="72"
-    >
-      <v-spacer></v-spacer>
-
-      <v-responsive max-width="156">
-        <v-text-field
-          dense
-          flat
-          hide-details
-          rounded
-          solo-inverted
-        ></v-text-field>
-      </v-responsive>
-    </v-app-bar>
-      <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        app
-        color="primary"
-        mini-variant
-      >
-        <v-avatar
-          class="d-block text-center mx-auto mt-4 pointer"
-          color="transparent"
-          size="36"
-        >
-        <img src="../assets/icons/hamburger.svg" style="max-width: 20px;" alt="">
-        </v-avatar>
-
-        <v-divider class="mx-3 my-5"></v-divider>
-
-        <v-avatar
-          v-for="n in 6"
-          :key="n"
-          class="d-block text-center mx-auto mb-9"
-          color="grey lighten-1"
-          size="28"
-        ></v-avatar>
-      </v-navigation-drawer>
-    <v-main>
-      <!--  -->
-    </v-main>
-  </v-app>
+  <div id="home">
+    <Sidebar />
+  </div>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar'
   export default {
     name: 'Home',
-    components: {},
+    components: {
+      Sidebar
+    },
     data: () => ({ drawer: null }),
   }
 </script>
