b5d25b2b01ebf2a89af470c9fd595796880c5b16
Muhammad Turmudzi <muhturmudzi10@gmail.com>
2020-11-03 11:28:53 +0700

    routing detail archieve

--- src/views/Dashboard/DetailArchieve.vue
+++ src/views/Dashboard/DetailArchieve.vue
@@ -5,44 +5,6 @@
         <Heading style="margin-right: 200px" />
         <Search />
       </div>
-      <div class="_left-starred">
-        <div class="_left-heading-starred">
-          <span>Starred Event</span>
-          <Button title="Add Event" @click="$refs.addEvent.visible = true" style="width: 115px; height: 39px;" :type="'outline-primary'">
-            <template slot="icon">
-              <img src="../../assets/icons/button/plus.svg">
-            </template>
-          </Button>
-        </div>
-        <div class="_left-heading-starred-card">
-          <div v-for="(item, index) in starred" :key="index">
-            <CardEvent :starred="item.starred" style="margin-bottom: 10px;" :title="item.title" :status="item.status">
-              <template slot="dropdown">
-                <span @click="toDetail('1')">Detail</span>
-                <span @click="item.starred = false">Unstar this event</span>
-              </template>
-            </CardEvent>
-          </div>
-          <span style="opacity: 0.5;" v-if="starred.length === 0">
-            There is no starred event.
-          </span>
-        </div>
-      </div>
-      <div class="_left-event-cards">
-        <div class="_left-event-cards-heading">
-          <span>Event Cards</span>
-        </div>
-        <div class="_left-event-cards-content">
-          <div v-for="(item, index) in notStarred" :key="index" style="margin-top: 10px;">
-            <CardEvent :starred="false" :title="item.title" :status="item.status">
-              <template slot="dropdown">
-                <span @click="toDetail('2')">Detail</span>
-                <span @click="item.starred = true">Star this event</span>
-              </template>
-            </CardEvent>
-          </div>
-        </div>
-      </div>
     </div>
     <div class="_right">
       <div class="_notification-slot">
@@ -76,33 +38,18 @@
         </div>
       </div>
     </div>
-
-    <Modal :title="'Add Event'" ref="addEvent">
-      <template slot="body">
-        <label class="custom-label" for="title">Event Title</label><br>
-        <input class="custom-input" type="text" id="title" name="title"><br>
-        <label class="custom-label" for="participants">Participants</label><br>
-        <input class="custom-input" type="text" id="participants" name="participants"><br>
-        <label class="custom-label" for="description">Description</label><br>
-        <textarea rows="100" class="custom-input" type="text-area" id="description" name="description"></textarea><br>
-      </template>
-
-      <template slot="footer">
-        <Button title="Save" type="primary" style="padding: 15px 25px;"></Button>
-      </template>
-    </Modal>
   </div>
 </template>
 
 <script>
 import Heading from '../../components/Heading'
 import Search from '../../components/atoms/Search'
-import Modal from '../../components/Modal'
+// import Modal from '../../components/Modal'
 import Notification from '../../components/Notification'
 
 // Atoms Components
-import Button from '../../components/atoms/Button'
-import CardEvent from '../../components/CardEvent'
+// import Button from '../../components/atoms/Button'
+// import CardEvent from '../../components/CardEvent'
 import Avatar from '../../components/atoms/Avatar'
 
 export default {
@@ -110,19 +57,14 @@ export default {
   components: {
     Heading,
     Search,
-    Button,
-    CardEvent,
-    Modal,
+    // Button,
+    // CardEvent,
+    // Modal,
     Notification,
     Avatar
   },
   data: () => ({
-    eventList: [
-      { status: 'cleared', title: 'Partnership dengan SpaceX', starred: true },
-      { status: 'on progress', title: 'Partnership dengan Tesla', starred: true },
-      { status: 'cleared', title: 'Partnership dengan Icon', starred: false },
-      { status: 'canceled', title: 'Partnership dengan Facebook', starred: false }
-    ]
+    
   }),
   computed: {
     starred () {
@@ -130,11 +72,6 @@ export default {
     },
     notStarred () {
       return this.eventList.filter(item => !item.starred)
-    }
-  },
-  methods: {
-    toDetail (val) {
-      this.$router.push('/archieve/' + val)
     }
   }
 
@@ -157,52 +94,6 @@ export default {
       flex-direction: row;
       width: 100%;
       justify-content: space-between;
-    }
-
-    ._left-starred {
-      margin-top: 52px;
-      display: flex;
-      flex-direction: column;
-      ._left-heading-starred {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        width: 100%;
-
-        span {
-          font-weight: bold;
-          font-size: 24px;
-          line-height: 33px;
-          /* identical to box height */
-
-          letter-spacing: 0.01em;
-
-          color: #333333;
-        }
-      }
-
-      ._left-heading-starred-card {
-        padding-top: 24px;
-      }
-    }
-
-    ._left-event-cards {
-      margin-top: 40px;
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-
-      ._left-event-cards-heading {
-        margin-bottom: 10px;
-        span {
-          font-weight: bold;
-          font-size: 24px;
-          line-height: 33px;
-          letter-spacing: 0.01em;
-
-          color: #333333;
-        }
-      }
     }
   }
   ._right {
