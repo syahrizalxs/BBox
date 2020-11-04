844c9ebc44d895f0e4f2fce7dca950ab1d45831e
Syahrizal Setiawan <syahrizal.setiawan11@gmail.com>
2020-11-04 17:04:52 +0700

    handle post for exchange token

--- src/views/RedirectGoogle.vue
+++ src/views/RedirectGoogle.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Please Wait</div>
+  <div></div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
-  name: 'Redirect-google'
+  name: 'Redirect-google',
+  async created () {
+    console.log(this)
+    this.$parent.$parent.isLoading = true
+    await this.loginSSO({ code: this.$route.query.code })
+    this.$parent.$parent.isLoading = false
+  },
+  methods: {
+    ...mapActions('auth', [
+      'loginSSO'
+    ])
+  }
 }
 </script>
 
