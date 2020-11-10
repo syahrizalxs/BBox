d2810e1327e74cb3e32ee84b806a71aea41036a4
Muhammad Turmudzi <muhturmudzi10@gmail.com>
2020-11-10 14:22:19 +0700

    update event sampe assign team

--- src/service/EventService.js
+++ src/service/EventService.js
@@ -4,7 +4,7 @@ export default class EventService extends HttpService {
   static api = process.env.VUE_APP_EVENT
 
   getEvent (param) {
-    const api = this.api + '/client/list'
+    const api = this.api + '/list'
     return this.get(param, api)
   }
 
@@ -15,6 +15,21 @@ export default class EventService extends HttpService {
 
   clientSubmit (param) {
     const api = this.api + '/client/submit'
+    return this.post(param, api)
+  }
+
+  acceptByManager (param) {
+    const api = this.api + '/manager/accept'
+    return this.get(param, api)
+  }
+
+  rejectByManager (param) {
+    const api = this.api + '/manager/reject'
+    return this.post(param, api)
+  }
+
+  assignTeam (param) {
+    const api = this.api + '/manager/add'
     return this.post(param, api)
   }
 
