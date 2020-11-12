80dac2e578b02f0aa86ee70231e817d6d59657ec
Syahrizal Setiawan <47208687+syahrizalxs@users.noreply.github.com>
2020-11-12 09:31:20 +0700

    Update Chart.vue
    Add more color to chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -19,7 +19,7 @@ export default {
         datasets: [
           {
             label: "Data One",
-            backgroundColor: ["#FF5964", "#FFE74C", "#6bf178", '#35a7ff', '#75dddd', '#aa3e98', '#9297c4'],
+            backgroundColor: ["#FF5964", "#FFE74C", "#6bf178", '#35a7ff', '#75dddd', '#aa3e98', '#9297c4', '#04151f', '#183a37', '#efd6ac', '#c44900', '#432534', '#ffe0b5', '#ede580', '#a4af69', '#4c2719', '#d35269', '#1a281f', '#c7b2c5'],
             data: this.data
           }
         ]
