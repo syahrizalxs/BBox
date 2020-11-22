94a8ff5cbde31f3dc4b070d711b3ab9e1022fd4d
Muhammad Turmudzi <muhturmudzi10@gmail.com>
2020-11-22 20:33:53 +0700

    update list process

--- src/service/ProcessService.js
+++ src/service/ProcessService.js
@@ -3,6 +3,11 @@ import HttpService from './HttpService'
 export default class ProcessService extends HttpService {
   static api = process.env.VUE_APP_PROCESS
 
+  getProcess(param) {
+  	const api = `${this.api}/list`
+  	return this.get(param, api)
+  }
+
   saveProcess(param) {
   	const api = `${this.api}/add`
   	return this.post(param, api)
